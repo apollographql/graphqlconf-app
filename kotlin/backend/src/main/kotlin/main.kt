a8c47ac62870f38522c38a4960e4d0956d74a59d
Martin Bonnin <martin@mbonnin.net>
2025-08-05 00:21:24 +0200

    trying more reverse proxy detection

--- kotlin/backend/src/main/kotlin/main.kt
+++ kotlin/backend/src/main/kotlin/main.kt
@@ -3,17 +3,18 @@ import com.apollographql.execution.ktor.apolloSandboxModule
 import com.apollographql.execution.ktor.apolloSubscriptionModule
 import graphqlconf.ServiceExecutableSchemaBuilder
 import io.ktor.http.*
-import io.ktor.server.application.install
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.forwardedheaders.ForwardedHeaders
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.forwardedheaders.*
 import io.ktor.server.request.*
 import io.ktor.server.util.*
-import io.ktor.util.toMap
+import io.ktor.util.*
 
 fun main(args: Array<String>) {
   embeddedServer(Netty, port = 8080) {
-    install(ForwardedHeaders)
+    install(XForwardedHeaders)
 
     val executableSchema = ServiceExecutableSchemaBuilder().build()
     // /graphql route
@@ -24,26 +25,8 @@ fun main(args: Array<String>) {
     apolloSandboxModule(title = "GraphQLConf mobile API", sandboxPath = "/") { call ->
 
       println("HEADERS: ${call.request.headers.toMap().map { "${it.key}=${it.value}" }.joinToString(",")}")
+      println("origin: ${call.request.origin.remoteHost}")
       call.url {
-        /**
-         * Trying to guess if the client connected through HTTPS
-         */
-        val proto = call.request.header("x-forwarded-proto")
-        when (proto) {
-          "http" -> protocol = URLProtocol.HTTP
-          "https" -> protocol = URLProtocol.HTTPS
-        }
-
-        if (port != 8080) {
-          /**
-           * We are not running locally
-           */
-          when (protocol) {
-            URLProtocol.HTTP -> port = 80
-            URLProtocol.HTTPS -> port = 443
-          }
-        }
-
         path("/graphql")
       }
     }
