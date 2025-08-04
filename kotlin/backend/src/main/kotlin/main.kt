c5515eaaa8b91c0067d6cc7cb8d7caab2111ae54
Martin Bonnin <martin@mbonnin.net>
2025-08-05 00:29:39 +0200

    cleanup

--- kotlin/backend/src/main/kotlin/main.kt
+++ kotlin/backend/src/main/kotlin/main.kt
@@ -6,14 +6,12 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.forwardedheaders.*
-import io.ktor.server.request.*
 import io.ktor.server.util.*
-import io.ktor.util.*
 
 fun main(args: Array<String>) {
   embeddedServer(Netty, port = 8080) {
+    // XForwardedHeaders allow us to get the origin url for the sandbox url
     install(XForwardedHeaders)
 
     val executableSchema = ServiceExecutableSchemaBuilder().build()
@@ -23,9 +21,6 @@ fun main(args: Array<String>) {
     apolloSubscriptionModule(executableSchema)
     // /sandbox/index.html route
     apolloSandboxModule(title = "GraphQLConf mobile API", sandboxPath = "/") { call ->
-
-      println("HEADERS: ${call.request.headers.toMap().map { "${it.key}=${it.value}" }.joinToString(",")}")
-      println("origin: ${call.request.origin.remoteHost}")
       call.url {
         path("/graphql")
       }
