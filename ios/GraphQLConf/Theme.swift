3ba5ba6f980d193722e710995afc7ea33402e0b2
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-08-06 15:38:31 -0700

    Move Font extension into Theme

--- ios/GraphQLConf/Theme.swift
+++ ios/GraphQLConf/Theme.swift
@@ -1,8 +1,25 @@
 import SwiftUI
 
-enum Theme: ShapeStyle {
-//  static let rhodamine = Color(hex: 0xFFE10098)
+extension Font {
 
+  enum HostGrotesk {
+    static let d1 = Font.custom("HostGrotesk-Regular", size: 48)
+    static let h1 = Font.custom("HostGrotesk-Regular", size: 40)
+    static let h2 = Font.custom("HostGrotesk-Regular", size: 32)
+    static let h3 = Font.custom("HostGrotesk-Regular", size: 24)
+    static let xxlarge = Font.custom("HostGrotesk-Regular", size: 20)
+    static let xlarge = Font.custom("HostGrotesk-Regular", size: 18)
+    static let large = Font.custom("HostGrotesk-Regular", size: 16)
+    static let medium = Font.custom("HostGrotesk-Regular", size: 14)
+    static let small = Font.custom("HostGrotesk-Regular", size: 12)
+    static let xsmall = Font.custom("HostGrotesk-Regular", size: 10)
+
+    static let navigationTitle = Font.custom("HostGrotesk-Medium", size: 24)
+  }
+  
+}
+
+enum Theme: ShapeStyle {
   case tint
   case tintReverse
   case navigationBar
@@ -38,6 +55,7 @@ enum Theme: ShapeStyle {
     #warning("Implement dark mode colors")
     return resolveLightColor()
   }
+
 }
 
 struct EventTypeColorMap {
