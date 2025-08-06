af32f880913df4cb7052b54492ddc1e3c8c8057d
Martin Bonnin <martin@mbonnin.net>
2025-08-06 18:37:55 +0200

    update code

--- kotlin/app/src/commonMain/kotlin/graphqlconf/app/misc/Schedule.kt
+++ kotlin/app/src/commonMain/kotlin/graphqlconf/app/misc/Schedule.kt
@@ -8,15 +8,11 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.apollographql.apollo.api.ApolloResponse
 import graphqlconf.api.GetScheduleItemsQuery
 import graphqlconf.app.DateTimeFormatting
-import graphqlconf.app.apolloClient
 import graphqlconf.design.component.DayHeader
 import graphqlconf.design.component.SessionCard
 import graphqlconf.design.theme.GraphqlConfTheme
@@ -35,12 +31,12 @@ fun Schedule(listState: LazyListState, response: ApolloResponse<GetScheduleItems
         this.items(response.data!!.scheduleItems) {
           when {
             it.onDayHeader != null -> {
-              DayHeader(it.onDayHeader.date, it.onDayHeader.title)
+              DayHeader(it.start.date, it.onDayHeader.title)
             }
             it.onTimeHeader != null -> {
               Text(
                 modifier = Modifier.padding(horizontal = 16.dp),
-                text = DateTimeFormatting.timeToTime(it.onTimeHeader.slotStart, it.onTimeHeader.slotEnd),
+                text = DateTimeFormatting.timeToTime(it.start.time, it.end.time),
                 color = GraphqlConfTheme.colors.primaryText,
                 style = GraphqlConfTheme.typography.h3,
               )
@@ -53,7 +49,7 @@ fun Schedule(listState: LazyListState, response: ApolloResponse<GetScheduleItems
                   eventType = session.event_type,
                   speakers = session.speakers.map { it.name },
                   venue = session.room?.name ?: "",
-                  time = DateTimeFormatting.timeToTime(session.start.time, session.end.time),
+                  time = DateTimeFormatting.timeToTime(it.start.time, it.end.time),
                   onClick = {},
                 )
               }
