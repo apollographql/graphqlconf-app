7dc8e311aa1584477db4fb897273922479488699
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-08-27 17:44:05 -0700

    fix: cache persistence

--- ios/GraphQLConf/Network.swift
+++ ios/GraphQLConf/Network.swift
@@ -1,11 +1,40 @@
 import Foundation
 import Apollo
+import ApolloSQLite
 
 class Network {
 
   static let shared = Network()
 
+  private let url = URL(string: "https://graphqlconf.app/graphql")!
+
   private(set) lazy var apollo: ApolloClient = {
-    ApolloClient(url: URL(string: "https://graphqlconf.app/graphql")!)
+    do {
+      let store: ApolloStore
+
+      if let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+        let documentsURL = URL(fileURLWithPath: documentsPath)
+        let sqliteFileURL = documentsURL.appendingPathComponent("apollo_db.sqlite")
+        let sqliteCache = try SQLiteNormalizedCache(fileURL: sqliteFileURL)
+        store = ApolloStore(cache: sqliteCache)
+
+      } else {
+        store = ApolloStore(cache: InMemoryNormalizedCache())
+      }
+
+      let networkTransport = RequestChainNetworkTransport(
+        urlSession: URLSession(configuration: .default),
+        interceptorProvider: DefaultInterceptorProvider.shared,
+        store: store,
+        endpointURL: self.url
+      )
+
+      return ApolloClient(networkTransport: networkTransport, store: store)
+
+    } catch {
+      // If for any reason we cannot use a persistent cache then move forward without it
+      return ApolloClient(url: self.url)
+    }
   }()
+
 }
