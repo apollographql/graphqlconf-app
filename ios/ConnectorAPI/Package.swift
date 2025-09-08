6b104ec6d2ef323e9bd45842726f7c4c0197c65a
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-09-08 15:50:10 -0700

    ios: config updates

--- ios/ConnectorAPI/Package.swift
+++ ios/ConnectorAPI/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.1
 
 import PackageDescription
 
@@ -21,5 +21,6 @@ let package = Package(
       ],
       path: "./Sources"
     ),
-  ]
+  ],
+  swiftLanguageModes: [.v6, .v5]
 )
