b0cb7a02899dd795cebe23feaafbf7ee5d9e0ed4
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-10-14 16:03:21 +0200

    evict bookmarks list on toggle

--- expo/src/apollo/bookmarksResolvers.ts
+++ expo/src/apollo/bookmarksResolvers.ts
@@ -1,12 +1,18 @@
 import type { LocalState } from "@apollo/client/local-state";
-import { getBookmarks, toggleBookmark, isBookmarked } from "@/utils/bookmarksStorage";
+import {
+  getBookmarks,
+  toggleBookmark,
+  isBookmarked,
+} from "@/utils/bookmarksStorage";
 
 export const bookmarksResolvers = {
   Query: {
     bookmarks: async (_root: unknown, args: { typename?: string }) => {
       const allBookmarks = await getBookmarks();
       if (args.typename) {
-        return allBookmarks.filter((bookmark) => bookmark.typename === args.typename);
+        return allBookmarks.filter(
+          (bookmark) => bookmark.typename === args.typename
+        );
       }
       return allBookmarks;
     },
@@ -15,13 +21,15 @@ export const bookmarksResolvers = {
   Mutation: {
     toggleBookmark: async (
       _root: unknown,
-      args: { id: string; typename: string; isBookmarked?: boolean }
+      args: { id: string; typename: string; isBookmarked?: boolean },
+      { client }
     ) => {
       const newState = await toggleBookmark(
         args.id,
         args.typename,
         args.isBookmarked
       );
+      client.cache.evict({ fieldName: "bookmarks" });
       return { __typename: args.typename, id: args.id, isBookmarked: newState };
     },
   },
