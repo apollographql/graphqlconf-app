2ec32c16e9234bb389688ce09d3ac4862b3f1f05
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-10-29 10:42:02 +0100

    harden mcp connection

--- expo/src/agent/mcp/supergraphMcp.ts
+++ expo/src/agent/mcp/supergraphMcp.ts
@@ -9,29 +9,41 @@ export async function getTools() {
       new URL(process.env.GRAPH_MCP_SERVER!)
     ),
   });
-  const tools = await supergraphMcpClient.tools();
-  return {
-    tools: {
-      ...tools,
-      getCurrentEvent: tool({
-        ...tools.GetEvents,
-        description: `Get details about the current event.`,
-        inputSchema: validatingJSONSchema({
-          type: "object",
-          properties: {},
-          additionalProperties: false,
+  try {
+    const tools = await supergraphMcpClient.tools();
+    return {
+      tools: {
+        ...tools,
+        getCurrentEvent: tool({
+          ...tools.GetEvents,
+          description: `Get details about the current event.`,
+          inputSchema: validatingJSONSchema({
+            type: "object",
+            properties: {},
+            additionalProperties: false,
+          }),
+          execute: (_, options) =>
+            tools.GetEvents.execute(
+              {
+                ids: [
+                  (options.experimental_context as AgentContext).currentEvent,
+                ],
+              },
+              options
+            ),
         }),
-        execute: (_, options) =>
-          tools.GetEvents.execute(
-            {
-              ids: [
-                (options.experimental_context as AgentContext).currentEvent,
-              ],
-            },
-            options
-          ),
-      }),
-    } as typeof tools,
-    close: () => supergraphMcpClient.close(),
-  };
+      } as typeof tools,
+      close: () => supergraphMcpClient.close(),
+    };
+  } catch (e) {
+    console.log(
+      "Error fetching MCP tools from %s: %o",
+      process.env.GRAPH_MCP_SERVER,
+      e
+    );
+    return {
+      tools: {},
+      close: () => supergraphMcpClient.close(),
+    };
+  }
 }
